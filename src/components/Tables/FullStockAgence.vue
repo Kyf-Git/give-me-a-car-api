f6310cb495a254783a564bd9360ea9b2372af418
franck <franck.isambert.pro@gmail.com>
2020-12-17 14:37:51 +0100

    ajoutVuetify

--- src/components/Tables/FullStockAgence.vue
+++ src/components/Tables/FullStockAgence.vue
@@ -11,6 +11,7 @@
     <tbody>
       <tr v-for="row in rows" v-bind:key="row" v-bind:row="row">
         <td>{{ row.id }}</td>
+        <td><v-btn>vouvou</v-btn></td>
         <td>{{ row.marque }}</td>
         <td>{{ row.km }}</td>
         <td>{{ row.disponibilité }}</td>
